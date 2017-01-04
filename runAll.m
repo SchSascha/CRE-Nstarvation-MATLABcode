dc583d6aec753bb34cc7227b3bc62c5728281c90
SchSascha <s.schaeuble@gmail.com>
2017-01-04 09:22:58 +0100

    More cleaning.

--- runAll.m
+++ runAll.m
@@ -8,28 +8,28 @@
 
 %
 % initialize COBRA toolbox if not done so already by calling
-% initCobraToolbox;
+initCobraToolbox;
 %
 
 modulePath = './data/ALGdL_modules.mat'; % change to whatever your path is
 
 % KO sims of single KO and module KO
 step4 = runAnalysis(4, [0.1 0.3 0.5]);
-
 % KD sims of single KD and module KD
 % step5 = runAnalysis(5, [0.1 0.3 0.5], [0.0625 0.125 0.25 0.5]);
+
 
 %
 % compute and visualize ratios of mod TAG & BM / wt TAG & BM
 % step4_ratio = visAnalysis(step4, 1,'step4');
-% step4mod_ratio = visAnalysis(step4mod, 1,'step4ppimod');
-
 % step5_ratio = visAnalysis(step5, 2,'step5');
 %
 
+
 % filter results for at least 5% change in TAG and/or BM production
-% filtSingles_step4     = filterRes(iRC1080, step4, 4, 1, 'singleKO');
-filtSingles_step4mod     = filterRes(iRC1080, step4mod, 4, 1, 'singleKO_ppiMod');
+% check filterRes for parameter description
+%
+filtSingles_step4     = filterRes(iRC1080, step4, 4, 1, 'singleKO');
 % filtSingles_step5bm10 = filterRes(iRC1080, step5, 5, 1, 'singleKD_BM10', 'lb10');
 % filtSingles_step5bm30 = filterRes(iRC1080, step5, 5, 1, 'singleKD_BM30', 'lb30');
 % filtSingles_step5bm50 = filterRes(iRC1080, step5, 5, 1, 'singleKD_BM50', 'lb50');
